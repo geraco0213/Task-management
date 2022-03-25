536274d82f522181d182d17dfff970ad1c752f72
EC2 Default User <ec2-user@ip-172-31-36-23.ap-northeast-1.compute.internal>
2022-03-25 13:44:13 +0000

    タスク一覧、作成ページ　途中

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   def index
+    @tasks =@user.tasks
   end
   
   def show 
@@ -7,6 +8,17 @@ class TasksController < ApplicationController
   
   def new
     @task=Task.new
+  end
+  
+  def create
+    @task= @user.tasks.build(task_params)
+    if @task.save
+      flash[:success]="タスクを新規作成しました"
+      redirect_to user_tasks_url @user
+    else
+      render new
+      
+    end
   end
   
 end
