1855b46c13bcefd87d968f94459ba53a510ffe5a
EC2 Default User <ec2-user@ip-172-31-36-23.ap-northeast-1.compute.internal>
2022-03-27 14:28:16 +0000

    あと少しで新規タスクの保存できそう

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
+  before_action :set_user
+  
   def index
-    @tasks =@user.tasks
+    @tasks=Task.where(user_id:@user.id)
   end
   
   def show 
@@ -8,18 +10,30 @@ class TasksController < ApplicationController
   
   def new
     @task=Task.new
+    
   end
   
   def create
     @task=@user.tasks.build(task_params)
     if @task.save
-      flash[:success]="タスクを新規作成しました"
+      flash[:success] = "タスクを新規作成しました。"
       redirect_to user_tasks_url @user
     else
-      render new
-      
+      render :new
     end
   end
+      
+  
+  
+  private
+  
+    def user_params
+      params.require(:task).permit(:name, :description, :user_id)
+    end
+  
+    def set_user
+      @user=User.find(params[:user_id])
+    end
   
 end
 
