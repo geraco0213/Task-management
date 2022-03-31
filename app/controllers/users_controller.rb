07b4e60e97373ceee3c1a2149626ad3fe5caae99
EC2 Default User <ec2-user@ip-172-31-36-23.ap-northeast-1.compute.internal>
2022-03-31 15:36:37 +0000

    フレンドリー追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user ,only:[:index,:show,:edit,:update]
   before_action :correct_user ,only:[:edit,:update]
   before_action :admin_user ,only: :index
-  before_action :admin_or_correct_user,only: :show
+ 
   
   
   def index
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -44,13 +44,16 @@ module SessionsHelper
     !current_user.nil?
   end
   
-  
   def redirect_back_or(default_url)
-    redirect_to(session:[:forwarding_url] ||default_url)
+    redirect_to(session[:forwarding_url] || default_url)
     session.delete(:forwarding_url)
   end
-  
+
+  # アクセスしようとしたURLを記憶します。
   def store_location
-    session[:forwarding_url]=request.original_url if request.get?
+    session[:forwarding_url] = request.original_url if request.get?
   end
+  
 end
+
+
