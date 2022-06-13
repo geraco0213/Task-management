bbe0c727d477f133541a32005b611516bacbc6c4
矢木敏樹 <yagitoshiki@yagitoshikinoMacBook-Air.local>
2022-06-13 16:23:41 +0900

    beforeのカッコ除いた

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user ,only:[:index,:show,:edit,:update,:destroy]
   before_action :correct_user ,only:[:edit,:update]
   before_action :admin_user ,only: [:index,:destroy]
-  before_action :admin_or_correct_user ,only: [:show]
+  before_action :admin_or_correct_user ,only: :show
  
   def index
     @users=User.paginate(page:params[:page],per_page:20)
